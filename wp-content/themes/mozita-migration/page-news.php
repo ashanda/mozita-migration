abbe7e5075f11a6c7e003312133ee66e6419c916
Sammani Jayaweera <sammanijayaweera0411@gmail.com>
2023-08-28 19:12:06 +0530

    bg imge update

--- wp-content/themes/mozita-migration/page-news.php
+++ wp-content/themes/mozita-migration/page-news.php
@@ -11,7 +11,7 @@ get_header(); // Include the header
 
 <!-- page-title -->
 <section class="page-title centred p_relative">
-    <div class="bg-layer" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/background/5.png);"></div>
+    <div class="bg-layer" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/background/16.png);"></div>
     <div class="pattern-layer" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/shape/shape-67.png);"></div>
     <div class="auto-container">
         <div class="content-box">
