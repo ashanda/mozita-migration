9b79c495bc45c65fc81010133d653b6adccb06cd
Sammani Jayaweera <sammanijayaweera0411@gmail.com>
2023-08-29 12:11:13 +0530

    about-us-page design

--- wp-content/themes/mozita-migration/page-services.php
+++ wp-content/themes/mozita-migration/page-services.php
@@ -30,16 +30,7 @@ get_header(); // Include the header
 <!-- coaching-section -->
 <section class="coaching-section pb_150">
     <div class="auto-container">
-        <div class="title-box mb_70">
-            <div class="sec-title">
-                <span class="sub-title">HOW WE HELP CLIENTS</span>
-                <h2>Your immigration class New Orleans Saints deserves you.</h2>
-            </div>
-            <div class="text">
-                <p>Where did this incorrect belief that everyone dislikes praising happiness come from? Explain to me how this erroneous theory ended up being devised.</p>
-            </div>
-        </div>
-        <div class="row clearfix">
+        <div class="row clearfix pt_100">
             <div class="col-lg-4 col-md-6 col-sm-12 coaching-block">
                 <div class="coaching-block-one">
                     <div class="inner-box">
@@ -48,11 +39,9 @@ get_header(); // Include the header
                             <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-2.jpg" alt=""></figure>
                         </div>
                         <div class="lower-content">
-                            <h3><a href="#">Oet Coaching</a></h3>
-                            <p>Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna suscipit, fermentum.</p>
-                            <div class="link-box">
-                                <a href="#"><span>Read More</span></a>
-                            </div>
+                            <h3><a href="#">Empowering Employers: Our Comprehensive Migration Solutions</a></h3>
+                            <p>In the dynamic landscape of global workforce mobility, finding the right talent for your business can be a transformative step. At Mozita Migration, we understand the challenges employers face in recruiting from overseas and navigating the intricacies of the employee visa process. Our employer services extend beyond just immigration support; we offer a holistic approach that encompasses recruiting, visa processing, accreditation, and ongoing support. In this guide, we will unveil the array of services we provide to employers, ensuring a seamless and successful international recruitment journey.</p>
+                            
                         </div>
                     </div>
                 </div>
@@ -65,11 +54,9 @@ get_header(); // Include the header
                             <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-4.jpg" alt=""></figure>
                         </div>
                         <div class="lower-content">
-                            <h3><a href="#">Pte Coaching</a></h3>
-                            <p>Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna suscipit, fermentum.</p>
-                            <div class="link-box">
-                                <a href="#"><span>Read More</span></a>
-                            </div>
+                            <h3><a href="#">Recruiting from Overseas: A World of Opportunities</a></h3>
+                            <p>The global talent pool is rich and diverse, and recruiting internationally can bring a fresh perspective and expertise to your team. Our team at Mozita Migration collaborates closely with employers to understand their specific needs, helping them tap into a vast network of skilled professionals across the globe. Whether you're seeking to fill skill shortages or bolster your team with unique talents, we connect you with qualified candidates who align with your business objectives.</p>
+                            
                         </div>
                     </div>
                 </div>
@@ -82,11 +69,57 @@ get_header(); // Include the header
                             <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-6.jpg" alt=""></figure>
                         </div>
                         <div class="lower-content">
-                            <h3><a href="#">Tofel Coaching</a></h3>
-                            <p>Sed gravida nisl a porta tincidunt. Integer aliquam nisi sit amet magna suscipit, fermentum.</p>
-                            <div class="link-box">
-                                <a href="#"><span>Read More</span></a>
-                            </div>
+                            <h3><a href="#">New Employee Visa Process: Navigating the Visa Landscape</a></h3>
+                            <p>Once you've identified the ideal candidate, the next step is navigating the complex world of employee visas. Our expert team guides you through every step of the process, ensuring compliance with New Zealand immigration regulations. We provide personalized advice, assist in preparing required documentation, and streamline the application process, alleviating the stress and uncertainty often associated with visa applications. With our guidance, you can confidently facilitate the entry of new employees into New Zealand.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-4 col-md-6 col-sm-12 coaching-block">
+                <div class="coaching-block-one">
+                    <div class="inner-box">
+                        <div class="image-box clearfix">
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-1.jpg" alt=""></figure>
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-2.jpg" alt=""></figure>
+                        </div>
+                        <div class="lower-content">
+                            <h3><a href="#">Employer Accreditation: Setting the Stage for Success</a></h3>
+                            <p>Obtaining employer accreditation is a strategic move that streamlines the process of bringing in international talent. Mozita Migration assists employers in achieving accreditation status, guiding them through the application process and ensuring all requirements are met. Our in-depth knowledge of accreditation criteria ensures that your business meets the necessary standards, enabling a smoother and faster visa process for prospective employees.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 coaching-block">
+                <div class="coaching-block-one">
+                    <div class="inner-box">
+                        <div class="image-box clearfix">
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-3.jpg" alt=""></figure>
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-4.jpg" alt=""></figure>
+                        </div>
+                        <div class="lower-content">
+                            <h3><a href="#">Employment Visa Issue Support: Ensuring Smooth Transitions</a></h3>
+                            <p>Transitioning to a new work environment can be both exciting and overwhelming for international employees. We recognize the importance of a seamless integration process and offer comprehensive support to help new hires settle into their roles and surroundings. From obtaining necessary documentation to providing orientation and support services, we ensure that your employees feel valued, supported, and ready to contribute from day one.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 coaching-block">
+                <div class="coaching-block-one">
+                    <div class="inner-box">
+                        <div class="image-box clearfix">
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-5.jpg" alt=""></figure>
+                            <figure class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-6.jpg" alt=""></figure>
+                        </div>
+                        <div class="lower-content">
+                            <h3><a href="#">Ongoing Support: Your Trusted Partner</a></h3>
+                            <p>At Mozita Migration, our commitment to employers doesn't end once the visa is granted. We remain your trusted partner throughout the employee's tenure, offering continuous support to address any immigration-related challenges that may arise. From visa renewals to adapting to changing regulations, we keep you informed and prepared, enabling your business to focus on its core operations while we handle the intricacies of immigration.</p>
+                            
                         </div>
                     </div>
                 </div>
@@ -105,20 +138,17 @@ get_header(); // Include the header
                 <div class="content_block_two">
                     <div class="content-box p_relative d_block">
                         <div class="sec-title mb_20">
-                            <span class="sub-title">Empowering Employers:</span>
-                            <h2>Our Comprehensive Migration Solutions</h2>
+                            <span class="sub-title">Benefits of Partnering with Mozita Migration</span>
+                            <h2>Our comprehensive suite of employer services offers numerous advantages:</h2>
                         </div>
-                        <div class="text mb_25">
+                        <!-- <div class="text mb_25">
                             <p>In the dynamic landscape of global workforce mobility, finding the right talent for your business can be a transformative step. At Mozita Migration, we understand the challenges employers face in recruiting from overseas and navigating the intricacies of the employee visa process. Our employer services extend beyond just immigration support; we offer a holistic approach that encompasses recruiting, visa processing, accreditation, and ongoing support. In this guide, we will unveil the array of services we provide to employers, ensuring a seamless and successful international recruitment journey.</p>
-                        </div>
+                        </div> -->
                         <ul class="list-item p_relative mb_40 clearfix">
-                            <li><a href="#">1. Entering & Leaving From Country <i class="icon-9"></i></a></li>
-                            <li><a href="#">2. Help & Support <i class="icon-9"></i></a></li>
-                            <li><a href="#">3. Country Citizenship <i class="icon-9"></i></a></li>
+                            <li><a href="#">1. <strong>Efficiency:</strong> Streamlined processes save your business time and resources. </a></li>
+                            <li><a href="#">2. <strong>Compliance:</strong> We ensure adherence to immigration regulations, mitigating potential risks. </a></li>
+                            <li><a href="#">3. <strong>Access to Talent:</strong> Our global network connects you with the right candidates for your business. </a></li>
                         </ul>
-                        <div class="btn-box">
-                            <a href="#" class="theme-btn"><span>More Details</span></a>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -132,95 +162,11 @@ get_header(); // Include the header
 </section>
 <!-- dream-style-three end -->
 
-
-<!-- coaching-style-three -->
-<section class="coaching-style-three">
-    <div class="outer-container clearfix">
-        <div class="coaching-block-three">
-            <div class="inner-box">
-                <div class="static-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-7.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-38"></i></div>
-                        <h3><a href="#">TOEFL Coaching</a></h3>
-                        <p>Equaly blame belongs those fail their all duty through weak shrinking.</p>
-                    </div>
-                </div>
-                <div class="overlay-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-7.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-38"></i></div>
-                        <h3><a href="#">TOEFL Coaching</a></h3>
-                        <p>Vivamus ac ultricies ex. Donec lacinia lacus libero, sed feugiat diam laoreet eget. Vivamus venenatis turpis lacinia, congue massa in, accumsan orci.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="coaching-block-three">
-            <div class="inner-box">
-                <div class="static-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-8.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-39"></i></div>
-                        <h3><a href="#">Graduate Re-Exam</a></h3>
-                        <p>Equaly blame belongs those fail their all duty through weak shrinking.</p>
-                    </div>
-                </div>
-                <div class="overlay-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-8.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-39"></i></div>
-                        <h3><a href="#">Graduate Re-Exam</a></h3>
-                        <p>Vivamus ac ultricies ex. Donec lacinia lacus libero, sed feugiat diam laoreet eget. Vivamus venenatis turpis lacinia, congue massa in, accumsan orci.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="coaching-block-three">
-            <div class="inner-box">
-                <div class="static-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-9.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-40"></i></div>
-                        <h3><a href="#">Scholastic Apti</a></h3>
-                        <p>Equaly blame belongs those fail their all duty through weak shrinking.</p>
-                    </div>
-                </div>
-                <div class="overlay-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-9.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-40"></i></div>
-                        <h3><a href="#">Scholastic Apti</a></h3>
-                        <p>Vivamus ac ultricies ex. Donec lacinia lacus libero, sed feugiat diam laoreet eget. Vivamus venenatis turpis lacinia, congue massa in, accumsan orci.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="coaching-block-three">
-            <div class="inner-box">
-                <div class="static-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-10.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-41"></i></div>
-                        <h3><a href="#">Management Test</a></h3>
-                        <p>Equaly blame belongs those fail their all duty through weak shrinking.</p>
-                    </div>
-                </div>
-                <div class="overlay-content">
-                    <figure class="image-box"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/resource/coaching-10.jpg" alt=""></figure>
-                    <div class="content-box">
-                        <div class="icon-box"><i class="icon-41"></i></div>
-                        <h3><a href="#">Management Test</a></h3>
-                        <p>Vivamus ac ultricies ex. Donec lacinia lacus libero, sed feugiat diam laoreet eget. Vivamus venenatis turpis lacinia, congue massa in, accumsan orci.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<section class="about-conclusion pb_150">
+    <div class="auto-container">
+        <p>In the ever-evolving landscape of global talent recruitment, partnering with Mozita Migration offers employers a strategic advantage. From identifying international talent to managing the intricacies of the employee visa process, our services empower businesses to harness the full potential of a diverse and skilled workforce. With our guidance, you can navigate the complexities of immigration with confidence, while we ensure that your business remains at the forefront of global talent acquisition. Let us be your trusted partner in building a successful, diverse, and dynamic team.</p>
     </div>
 </section>
-<!-- coaching-style-three end -->
-
-
 
 
 <?php
